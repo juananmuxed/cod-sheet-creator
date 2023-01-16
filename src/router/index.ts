f5435bef5a14c567836b19c1b17a95ee2329e3d5
MuXeD <juanan.sm87@gmail.com>
2023-01-16 20:04:29 +0100

    💡 Delete TODO comment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-// FIXME: pass id between views
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
