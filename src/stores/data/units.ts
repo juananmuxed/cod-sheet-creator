3c80085a84bd90c1ce45c723e9250f662ed87671
MuXeD <juanan.sm87@gmail.com>
2023-01-18 22:25:40 +0100

    🗃️ Add the Gallics

--- src/stores/data/units.ts
+++ src/stores/data/units.ts
@@ -1,9 +1,11 @@
 import characters from "./units/characters.json";
 import cartaginian from "./units/cartaginian.json";
+import gallic from "./units/gallic.json";
 
 // TODO: finish imported database
 
 export default {
   ...characters,
   ...cartaginian,
+  ...gallic,
 };
