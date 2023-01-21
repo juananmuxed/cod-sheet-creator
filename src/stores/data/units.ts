e14719d7ce0701d2e5f2422cb686a7ce1241a282
MuXeD <juanan.sm87@gmail.com>
2023-01-21 12:02:25 +0100

    🗃️ Add Roman army

--- src/stores/data/units.ts
+++ src/stores/data/units.ts
@@ -4,6 +4,9 @@ import gallic from "./units/gallic.json";
 import greek from "./units/greek.json";
 import iberian from "./units/iberian.json";
 import italian from "./units/italian.json";
+import roman from "./units/roman.json";
+import numidian from "./units/numidian.json";
+import pyrro from "./units/pyrro.json";
 
 export default {
   ...characters,
@@ -12,4 +15,7 @@ export default {
   ...greek,
   ...iberian,
   ...italian,
+  ...roman,
+  ...numidian,
+  ...pyrro,
 };
